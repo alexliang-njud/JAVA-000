bc75afc47770f232e92a5a6f38e934420eca3bd0
alexliang-njud <626088245@qq.com>
2020-12-04 14:52:38 +0800

    第7次作业

---
+++ Week_07/src/main/java/org/example/MultiThreadSaveDataByHaikari.java
@@ -0,0 +1,58 @@
+package org.example;
+
+import org.apache.commons.lang3.time.StopWatch;
+import org.apache.juli.logging.Log;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.sql.SQLException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@SpringBootApplication
+public class MultiThreadSaveDataByHaikari implements CommandLineRunner {
+    private final ExecutorService executor = Executors.newFixedThreadPool(16);
+    private static final Logger LOGGER = LoggerFactory.getLogger(MultiThreadSaveDataByHaikari.class);
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    AtomicInteger count = new AtomicInteger(1);
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiThreadSaveDataByHaikari.class);
+    }
+
+
+    private void batchsaveData() throws SQLException {
+        StopWatch stopWatch = StopWatch.createStarted();
+        for(int i = 0;i<1000;i++){
+            jdbcTemplate.execute("insert  into t_user_0 (id, name, address) values(" + (count.getAndIncrement()) + ", 'test', '杭州')");
+        }
+        stopWatch.stop();
+        LOGGER.info("foreach execution time: {} ms" , stopWatch.getTime(TimeUnit.MILLISECONDS));
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        LOGGER.info("batch insert data start......");
+        for (int i = 0 ; i< 1000;i++){
+            executor.execute(() ->{
+                try {
+                    batchsaveData();
+                } catch (SQLException e) {
+                    LOGGER.error("execute failed, error is {}", e);
+                }
+            });
+        };
+        executor.shutdown();
+
+    }
+}
---
+++ Week_07/src/main/java/org/example/MultiThreadSaveDataDemo.java
@@ -0,0 +1,58 @@
+package org.example;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.time.StopWatch;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Slf4j
+public class MultiThreadSaveDataDemo {
+    private static final ExecutorService executor = Executors.newFixedThreadPool(16);
+    private static final Logger LOGGER = LoggerFactory.getLogger(MultiThreadSaveDataDemo.class);
+
+    private static AtomicInteger count = new AtomicInteger(1);
+
+    public static void main(String[] args) throws SQLException {
+        //通过工具类获取数据库连接对象
+        Connection con = JdbcUtil.getConnection();
+
+        for (int i = 0; i < 1000; i++) {
+            executor.execute(() -> {
+                saveData(con);
+            });
+            LOGGER.info("call save method"+i);
+        };
+
+        // close connection
+    }
+
+    private static void saveData(Connection connection) {
+        try {
+            StopWatch stopWatch = StopWatch.createStarted();
+
+            String insertSQL = "insert into t_user_1 (id, name, address) values(?,?,?)";
+            PreparedStatement pst = connection.prepareStatement(insertSQL);
+            for (int i = 0; i < 10000; i++) {
+                pst.setInt(1, count.getAndIncrement());
+                pst.setString(2, "test" + i);
+                pst.setString(3, "杭州");
+                pst.addBatch();
+            }
+            pst.executeBatch();
+
+            JdbcUtil.close(pst, null);
+            stopWatch.stop();
+            LOGGER.info("thread execution time: {} ms" , stopWatch.getTime(TimeUnit.MILLISECONDS));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
